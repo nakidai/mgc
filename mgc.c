3a22f8158aae2f39d6d9ba1176a2db2545b21864
Plaza521 <alex_platonov1@mail.ru>
2023-06-04 18:27:04 +0300

    Attempt to use 1d array

--- mgc.c
+++ mgc.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
-#include <stdarg.h>
 
 #define MINE 10
 
@@ -12,24 +11,27 @@ void usage(void)
 	exit(1);
 }
 
-uint8_t getElementFrom2DArray8(uint8_t *field, int x, int y)
+#define free2DArray(x, y) _free2DArray((void **)x, y)
+void _free2DArray(void **arr, uint32_t size)
 {
-	return *(field + (y*sizeof(field)) + x);
+	for(uint32_t i = 0; i < size; ++i)
+		free(arr[i]);
+	free(arr);
 }
 
-void generateField(uint8_t *field, int mines_amount, int size)
+void generateField(uint8_t **field, int mines_amount, int size)
 {
 	uint8_t mines_left = mines_amount;
 	uint8_t x;
 	uint8_t y;
 
 	while (mines_left != 0)
-	{ 
+	{
 		x = rand() % size;
 		y = rand() % size;
-		if (getElementFrom2DArray8(field, y, x) == MINE)
+		if (field[y][x] == MINE)
 			continue;
-		getElementFrom2DArray8(field, y, x) += MINE;
+		field[y][x] += MINE;
 		for (int8_t i = -1; i < 2; ++i)
 			for (int8_t j = -1; j < 2; ++j)
 			{
@@ -37,7 +39,7 @@ void generateField(uint8_t *field, int mines_amount, int size)
 				int8_t xr = x+i;
 				if (yr < 0 || xr < 0 || yr >= size || xr >= size)
 					continue;
-				++getElementFrom2DArray8(field, yr, xr);
+				++field[yr][xr];
 			}
 		--mines_left;
 	}
@@ -50,9 +52,9 @@ int main(int argc, char **argv)
 		usage();
 	srand(time(NULL));
 
-	uint8_t (*field)[size] = malloc(sizeof(uint8_t[size][size]));
-	// for(uint8_t i = 0; i < size; i++)
-  //   field[i] = malloc(size * sizeof(*field[i]));
+	uint8_t **field = malloc(size * sizeof(*field));
+	for(uint8_t i = 0; i < size; i++)
+    field[i] = malloc(size * sizeof(*field[i]));
 	for (uint8_t i = 0; i < size; ++i)
 		for (uint8_t j = 0; j < size; ++j)
 			field[j][i] = 0;
@@ -63,16 +65,16 @@ int main(int argc, char **argv)
 	{
 		for (uint8_t j = 0; j < size; ++j)
 		{
-			if (getElementFrom2DArray8(field, j, i) >= MINE)
+			if (field[j][i] >= MINE)
 			{
 				printf("||`BM`||");
 				continue;
 			}
-			printf("||` %d`||", getElementFrom2DArray8(field, j, i));
+			printf("||` %d`||", field[j][i]);
 		}
 		printf("\n");
 	}
 
-	free(field);
+	free2DArray(field, size);
 	exit(0);
 }
